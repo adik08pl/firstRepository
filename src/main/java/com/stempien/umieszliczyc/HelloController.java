189cf87134b74a37c12549c7776540b661480010
adik08pl <adik08.email@gmail.com>
2022-10-22 15:12:18 +0200

    Pierwszy projekt

--- src/main/java/com/stempien/umieszliczyc/HelloController.java
+++ src/main/java/com/stempien/umieszliczyc/HelloController.java
@@ -1,14 +1,49 @@
 package com.stempien.umieszliczyc;
 
-import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.Random;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    public Label lblPierwszaLiczba;
+    public Label lblDrugaLiczba;
+    public Label lblZnak;
+    public TextField txtPisz;
+    public Label lblPunkty;
+    public Button btnNastepne;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void btnNastepneClick(ActionEvent actionEvent) {
+        int punkty = 0;
+        Random random = new Random();
+        int liczba1 = random.nextInt(99) + 1;
+        lblPierwszaLiczba.setText("" + liczba1);
+        int liczba2 = random.nextInt(99) + 1;
+        lblDrugaLiczba.setText("" + liczba2);
+        int dzialanie = random.nextInt(3) + 1;
+        int wynik = 0;
+        if (dzialanie == 1) {
+            lblZnak.setText("+");
+            wynik = liczba1 + liczba2;
+        }
+        else if (dzialanie == 2) {
+           lblZnak.setText("-");
+            wynik = liczba1 - liczba2;
+        }
+        else if (dzialanie == 3) {
+            lblZnak.setText("*");
+            wynik = liczba1 * liczba2;
+        }
+       String wynikUzykownikaTxt = txtPisz.getText();
+        int wynikUzykownikaLiczba = Integer.parseInt(wynikUzykownikaTxt);
+if (wynik == wynikUzykownikaLiczba){
+    punkty = punkty + 5;
+}
+    else{
+        punkty = punkty - 10;
+    }
+    lblPunkty.setText("" + punkty);
     }
 }
